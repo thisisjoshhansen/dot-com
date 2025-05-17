e149737c1b23524d66e9164d841b10d2c4bba488
Josh Hansen <joshhans@cisco.com>
2025-05-17 16:03:23 -0600

    mono-size font!

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>thisisjoshhansen</div>
-      <div>42</div>
+      <div className="title">thisisjoshhansen</div>
     </>
   );
 }
