2df5bdcbb7ffd481b5eaf7760f5297e4b628f63b
Josh Hansen <joshhans@cisco.com>
2025-05-17 16:32:08 -0600

    Added brackets

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="title">thisisjoshhansen</div>
+      <div className="title">[ thisisjoshhansen ]</div>
     </>
   );
 }
